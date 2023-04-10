7fa8bdb76a2edfc37b0ddc5e197a1bd156c78956
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-09 22:19:13 -0400

    updating colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
-import { TailcastLogo } from "../assets/logos/TailcastLogo";
-import { GithubIcon } from "../assets/icons/GithubIcon";
+import { TailcastLogo } from '../assets/logos/TailcastLogo';
+import { GithubIcon } from '../assets/icons/GithubIcon';
 
 const navbarLinks = [
-  { label: "Home", href: "#home", ariaLabel: "Home" },
-  { label: "Features", href: "#features", ariaLabel: "Features" },
-  { label: "Pricing", href: "#pricing", ariaLabel: "Pricing" },
-  { label: "FAQ", href: "#FAQ", ariaLabel: "FAQ" },
+  { label: 'Home', href: '#home', ariaLabel: 'Home' },
+  { label: 'Features', href: '#features', ariaLabel: 'Features' },
+  { label: 'Pricing', href: '#pricing', ariaLabel: 'Pricing' },
+  { label: 'FAQ', href: '#FAQ', ariaLabel: 'FAQ' },
 ];
 
 export const Navbar = () => {
@@ -21,8 +21,7 @@ export const Navbar = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3 }}
-          exit={{ opacity: 0 }}
-        >
+          exit={{ opacity: 0 }}>
           <a className="navbar-link" href="#home" aria-label="Home">
             <div className="flex justify-start items-center grow basis-0">
               <div className="text-white mr-2 text-6xl">
@@ -38,16 +37,14 @@ export const Navbar = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3 }}
-          exit={{ opacity: 0 }}
-        >
+          exit={{ opacity: 0 }}>
           <div className="hidden lg:flex h-full pl-12 pb-2">
             {navbarLinks.map(({ href, label, ariaLabel }) => (
               <a
                 className="navbar-link"
                 href={href}
                 aria-label={ariaLabel}
-                key={label}
-              >
+                key={label}>
                 {label}
               </a>
             ))}
@@ -57,25 +54,22 @@ export const Navbar = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3 }}
-          exit={{ opacity: 0 }}
-        >
+          exit={{ opacity: 0 }}>
           <div className="grow basis-0 justify-end hidden lg:flex">
             <a
               className="text-white custom-border-gray rounded-xl
            bg-customDarkBg2 hover:bg-customDarkBg3  border-gray-700 pl-6 pr-8 pt-2 pb-2 text-sm flex"
               href="https://github.com/matt765/Tidestream"
               target="_blank"
-              aria-label="source code"
-            >
+              aria-label="source code">
               <GithubIcon />
-              <span className="pt-px">Source code</span>
+              <span className="pt-px">Contact Us</span>
             </a>
           </div>
         </motion.div>
         <div
           className="lg:hidden flex flex-col  px-2 py-3 border-solid border border-gray-600 rounded-md cursor-pointer hover:bg-customDarkBg2"
-          onClick={() => setIsOpen(!isOpen)}
-        >
+          onClick={() => setIsOpen(!isOpen)}>
           <div className="w-5 h-0.5 bg-gray-500  mb-1"></div>
           <div className="w-5 h-0.5 bg-gray-500  mb-1"></div>
           <div className="w-5 h-0.5 bg-gray-500 "></div>
@@ -88,21 +82,18 @@ export const Navbar = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.3 }}
-            exit={{ opacity: 0 }}
-          >
+            exit={{ opacity: 0 }}>
             <div
               className="flex flex-col mt-16 lg:hidden absolute top-4 left-0  bg-customDarkBg1 z-50 w-full 
         items-center gap-10 pb-10 border-y border-solid border-customDarkBg3 pt-10
-        "
-            >
+        ">
               {navbarLinks.map(({ label, href, ariaLabel }) => (
                 <a
                   key={href}
                   className="navbar-link"
                   href={href}
                   onClick={() => setIsOpen(false)}
-                  aria-label={ariaLabel}
-                >
+                  aria-label={ariaLabel}>
                   {label}
                 </a>
               ))}
@@ -110,10 +101,9 @@ export const Navbar = () => {
                 className="text-white custom-border-gray rounded-xl
            bg-customDarkBg2 hover:bg-customDarkBg3  border-gray-700 pl-6 pr-8 pt-2 pb-2 text-sm flex"
                 href="https://github.com/matt765/Tidestream"
-                target="_blank"
-              >
+                target="_blank">
                 <GithubIcon />
-                Source code
+                Contact Us
               </a>
             </div>
           </motion.div>
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 
-import { InvitationModal } from "./InvitationModal";
-import { CheckArrowIcon } from "../assets/icons/CheckArrowIcon";
+import { InvitationModal } from './InvitationModal';
+import { CheckArrowIcon } from '../assets/icons/CheckArrowIcon';
 
 const pricingData = [
-  "Vestibulum viverra",
-  "Morbi mollis metus pretium",
-  "Etiam lectus nunc, commodo",
-  "Ut quam nisl mollis id pretium",
-  "Suspendisse bibendum",
+  'Vestibulum viverra',
+  'Morbi mollis metus pretium',
+  'Etiam lectus nunc, commodo',
+  'Ut quam nisl mollis id pretium',
+  'Suspendisse bibendum',
 ];
 
 export const Pricing = () => {
@@ -28,54 +28,30 @@ export const Pricing = () => {
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-        >
+          transition={{ duration: 0.5, delay: 0.2 }}>
           <div className="container mx-auto px-4">
             <div className="max-w-2xl mx-auto text-center mb-16">
               <span className="custom-block-subtitle">
                 Dolor sit amet consectutar
               </span>
               <h2 className="mt-6 mb-6 text-4xl lg:text-5xl font-bold font-heading text-white">
-                Choose your best plan
+                Packages for every budget
               </h2>
               <p className="mb-6 text-customGrayText">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
-              <label className="mx-auto bg-customDarkBg3 relative flex justify-between items-center group text-xl w-44 h-12 rounded-lg pr-36 pl-1 cursor-pointer">
-                <input
-                  type="checkbox"
-                  className="peer appearance-none"
-                  checked={!isMonthly}
-                  onChange={handleChange}
-                />
-                <span className="h-8 w-[5.5rem] flex items-center pr-2 bg-customDarkBg3 after:rounded-lg duration-300 ease-in-out  after:w-[30rem] after:h-10  after:bg-customPrimary   after:shadow-md after:duration-300 peer-checked:after:translate-x-[5.5rem] cursor-pointer"></span>
-                <div className="flex absolute text-white text-sm font-bold">
-                  <div
-                    className={
-                      isMonthly ? "mr-9 ml-3" : "mr-9 ml-3 text-gray-400"
-                    }
-                  >
-                    Monthly
-                  </div>
-                  <div className={isMonthly && "text-gray-400"}>Yearly</div>
-                </div>
-              </label>
             </div>
             <div className="flex flex-wrap flex-col lg:flex-row -mx-4 items-center mt-20">
               <div className="w-[350px] sm:w-[380px] lg:w-1/3 px-4 mb-8 lg:mb-0">
                 <div className="p-8 bg-customDarkBg3 rounded-3xl">
                   <h4 className="mb-2 text-xl font-bold font-heading text-white text-left">
-                    Beginner
+                    Single Page Site
                   </h4>
                   <div className="flex justify-start items-end">
                     <div className="text-4xl sm:text-5xl font-bold text-white text-left mt-4 mr-2">
-                      $0
-                    </div>
-                    <div className="text-gray-500">
-                      {isMonthly ? "/ month" : "/ year"}
+                      $400
                     </div>
                   </div>
-
                   <p className="mt-4 mb-6 2xl:mb-10 text-gray-500 leading-loose text-left">
                     The perfect way to get started and get used to our tools.
                   </p>
@@ -89,8 +65,7 @@ export const Pricing = () => {
                   </ul>
                   <div
                     className="inline-block text-center py-2 px-4 w-full rounded-xl rounded-t-xl custom-button-colored font-bold leading-loose mt-16"
-                    onClick={() => setIsModalOpen(true)}
-                  >
+                    onClick={() => setIsModalOpen(true)}>
                     Get Started
                   </div>
                 </div>
@@ -98,14 +73,11 @@ export const Pricing = () => {
               <div className="w-[350px] sm:w-[380px] lg:w-1/3 px-4 mb-8 lg:mb-0">
                 <div className="px-8 py-8 bg-customDarkBg3 rounded-3xl">
                   <h4 className="mb-2 2xl:mb-4 text-2xl font-bold font-heading text-white text-left">
-                    Standard
+                    Multi Page Site
                   </h4>
                   <div className="flex justify-start items-end">
                     <div className="text-4xl sm:text-5xl font-bold text-white text-left mt-4 mr-2">
-                      {isMonthly ? "$19" : "$180"}
-                    </div>
-                    <div className="text-gray-500">
-                      {isMonthly ? "/ month" : "/ year"}
+                      $1000
                     </div>
                   </div>
                   <p className="mt-8 mb-8 2xl:mb-12 text-gray-500 leading-loose text-left">
@@ -121,8 +93,7 @@ export const Pricing = () => {
                   </ul>
                   <div
                     className="inline-block text-center py-2 px-4 w-full custom-button-colored leading-loose transition duration-200 mt-20"
-                    onClick={() => setIsModalOpen(true)}
-                  >
+                    onClick={() => setIsModalOpen(true)}>
                     Get Started
                   </div>
                 </div>
@@ -130,14 +101,11 @@ export const Pricing = () => {
               <div className="w-[350px] sm:w-[380px] lg:w-1/3 px-4 mb-8 lg:mb-0">
                 <div className="p-8 bg-customDarkBg3 rounded-3xl">
                   <h4 className="mb-2 text-xl font-bold font-heading text-white text-left">
-                    Premium
+                    Enterprise
                   </h4>
                   <div className="flex justify-start items-end">
                     <div className="text-4xl sm:text-5xl font-bold text-white text-left mt-4 mr-2">
-                      {isMonthly ? "$36" : "$390"}
-                    </div>
-                    <div className="text-gray-500">
-                      {isMonthly ? "/ month" : "/ year"}
+                      Custom pricing
                     </div>
                   </div>
                   <p className="mt-4 mb-6 2xl:mb-10 text-gray-500 leading-loose text-left">
@@ -153,8 +121,7 @@ export const Pricing = () => {
                   </ul>
                   <div
                     className="inline-block text-center py-2 px-4 w-full rounded-xl rounded-t-xl custom-button-colored font-bold leading-loose mt-16"
-                    onClick={() => setIsModalOpen(true)}
-                  >
+                    onClick={() => setIsModalOpen(true)}>
                     Get Started
                   </div>
                 </div>
