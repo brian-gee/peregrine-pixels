8883ffb3434ffd9ed568f27795850e5db92571b9
dopecello <williamlocke.cello@gmail.com>
2023-05-02 20:41:39 -0400

    adjusted navbar content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
 
   return (
     <nav className="w-full h-20 flex flex-col justify-center items-center fixed bg-customDarkBg1 lg:bg-customDarkBgTransparent z-40 lg:backdrop-blur-xl">
-      <div className="2xl:w-[1280px] xl:w-10/12 w-11/12 flex justify-between items-center relative">
+      <div className="flex justify-between w-full items-center relative">
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -27,7 +27,7 @@ export const Navbar = () => {
               <div className="text-white mr-2 text-6xl">
                 <PeregrinePixelsLogo />
               </div>
-              <div className="text-white font-['Inter'] font-bold text-xl">
+              <div className="text-white font-['Inter'] font-bold text-xl ml-[1.7rem]">
                 Peregrine Pixels
               </div>
             </div>
@@ -38,7 +38,7 @@ export const Navbar = () => {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3 }}
           exit={{ opacity: 0 }}>
-          <div className="hidden lg:flex h-full pl- pb-2">
+          <div className="hidden lg:flex h-full mr-[7.5rem] xl:mr-[8.8rem]">
             {navbarLinks.map(({ href, label, ariaLabel }) => (
               <a
                 className="navbar-link"
@@ -57,8 +57,7 @@ export const Navbar = () => {
           exit={{ opacity: 0 }}>
           <div className="grow basis-0 justify-end hidden lg:flex">
             <a
-              className="custom-button-colored rounded-xl
-            pl-6 pr-8 pt-2 pb-2 text-sm flex"
+              className="custom-button-colored rounded-xl px-6 py-2 text-sm flex mr-8"
               href=""
               target="_blank"
               aria-label="source code">
@@ -67,7 +66,7 @@ export const Navbar = () => {
           </div>
         </motion.div>
         <div
-          className="lg:hidden flex flex-col  px-2 py-3 border-solid border border-gray-600 rounded-md cursor-pointer hover:bg-customDarkBg2"
+          className="lg:hidden flex flex-col px-2 py-3 border-solid border border-gray-600 rounded-md cursor-pointer hover:bg-customDarkBg2 mr-[0.8rem]"
           onClick={() => setIsOpen(!isOpen)}>
           <div className="w-5 h-0.5 bg-gray-500  mb-1"></div>
           <div className="w-5 h-0.5 bg-gray-500  mb-1"></div>
@@ -83,9 +82,7 @@ export const Navbar = () => {
             transition={{ duration: 0.3 }}
             exit={{ opacity: 0 }}>
             <div
-              className="flex flex-col mt-16 lg:hidden absolute top-4 left-0  bg-customDarkBg1 z-50 w-full 
-        items-center gap-10 pb-10 border-y border-solid border-customDarkBg3 pt-10
-        ">
+              className="flex flex-col mt-16 lg:hidden absolute top-4 left-0  bg-customDarkBg1 z-50 w-full items-center gap-10 pb-10 border-y border-solid border-customDarkBg3 pt-10">
               {navbarLinks.map(({ label, href, ariaLabel }) => (
                 <a
                   key={href}
@@ -97,8 +94,7 @@ export const Navbar = () => {
                 </a>
               ))}
               <a
-                className="text-white custom-border-gray rounded-xl
-           bg-customDarkBg2 hover:bg-customDarkBg3  border-gray-700 pl-6 pr-8 pt-2 pb-2 text-sm flex"
+                className="text-white custom-border-gray rounded-xl bg-customDarkBg2 hover:bg-customDarkBg3  border-gray-700 pl-6 pr-8 pt-2 pb-2 text-sm flex"
                 href=""
                 target="_blank">
                 Contact Us
